9e83b3c6eb4e963aa5d831b7075a9637c4b71377
Sergei <serge-mur@yandex.ru>
2019-02-25 14:31:31 +0300

    Add sticky navbar

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,46 @@ $(document).ready(function () {
         triggerButton: '#triggerButton'
     });
 
-    if (window.matchMedia("screen and (min-width: 1px) and (max-width:575px)").matches) {
-        console.log('mobile');
+    if (window.matchMedia("screen and (min-width: 1px) and (max-width:991px)").matches) {
+        // console.log('mobile');
+        if($('#sidebar .collapse').hasClass('show')) {
+            $('#sidebar .collapse').removeClass('show');
+        }
     }
+
+// Sticky navbar
+// =========================
+
+    // Custom function which toggles between sticky class (is-sticky)
+    var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
+        var stickyHeight = sticky.outerHeight();
+        var stickyTop = stickyWrapper.offset().top;
+        if (scrollElement.scrollTop() >= stickyTop) {
+            stickyWrapper.height(stickyHeight);
+            sticky.addClass("is-sticky");
+        }
+        else {
+            sticky.removeClass("is-sticky");
+            stickyWrapper.height('auto');
+        }
+    };
+
+    // Find all data-toggle="sticky-onscroll" elements
+    $('[data-toggle="sticky-onscroll"]').each(function () {
+        var sticky = $(this);
+        var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+        sticky.before(stickyWrapper);
+        sticky.addClass('sticky');
+
+        // Scroll & resize events
+        $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
+            stickyToggle(sticky, stickyWrapper, $(this));
+        });
+
+        // On page load
+        stickyToggle(sticky, stickyWrapper, $(window));
+    });
+
+
 
 });
